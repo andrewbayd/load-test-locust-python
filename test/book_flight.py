9c61d59a3d2bd8a313595698f7d48eafe96fc049
Andrii Baidachenko <andrewbayd@gmail.com>
2020-07-14 23:40:22 +0200

    refactor tasks

--- test/book_flight.py
+++ test/book_flight.py
@@ -7,82 +7,92 @@ from helper import verify_response_time
 
 class UserBehaviour(SequentialTaskSet):
 
-    # disabled since this functionality is not working on demo site
-    # def on_start(self):
-    #     print("Login to account")
-    #     payload = {
-    #         "action": "process",
-    #         "userName": "andriitest",
-    #         "password": "locusttest",
-    #         "login.x": "41",
-    #         "login.y": "12"
-    #     }
-    #     response = self.client.post("/login.php", name="login", data=payload)
-    #     assert response.status_code is HTTPStatus.OK, "Failed to login"
-    #     assert verify_response_time(response, 2), "Login request took more than 2 second"
+    def on_start(self):
+        print("Login to account")
+        response = self.client.post(
+            "/login.php",
+            data={
+                "action": "process",
+                "userName": "andriitest",
+                "password": "locusttest",
+                "login.x": "41",
+                "login.y": "12"
+            },
+            name="login")
+        assert response.status_code is HTTPStatus.OK, "Failed to login"
+        assert verify_response_time(response, 2), "Login request took more than 2 second"
 
     @task
     def find_flight(self):
         print("Perform request to find a flight")
-        payload = {
-            "tripType": "roundtrip",
-            "passCount": 1,
-            "fromPort": "Acapulco",
-            "fromMonth": 7,
-            "fromDay": 12,
-            "toPort": "Acapulco",
-            "toMonth": 7,
-            "toDay": 12,
-            "servClass": "Coach",
-            "airline": "No Preference",
-            "findFlights.x": 59,
-            "findFlights.y": 13
-        }
-        response = self.client.post("/mercuryreservation2.php", name="Find a flight", data=payload)
+        response = self.client.post(
+            "/mercuryreservation2.php",
+            data={
+                "tripType": "roundtrip",
+                "passCount": 1,
+                "fromPort": "Acapulco",
+                "fromMonth": 7,
+                "fromDay": 12,
+                "toPort": "Acapulco",
+                "toMonth": 7,
+                "toDay": 12,
+                "servClass": "Coach",
+                "airline": "No Preference",
+                "findFlights.x": 59,
+                "findFlights.y": 13
+            },
+            name="Find a flight")
         assert response.status_code == HTTPStatus.OK, "Failed to find a flight"
         assert verify_response_time(response, 1), "Request to find a flight took more than 1 second"
 
     @task
     def select_flight(self):
         print("Perform request to select a flight")
-        payload = {
-            "fromPort": "Acapulco",
-            "toPort": "Acapulco",
-            "passCount": 1,
-            "toDay": 12,
-            "toMonth": 7,
-            "fromDay": 12,
-            "fromMonth": 7,
-            "servClass": "Coach",
-            "outFlight": "Pangea Airlines$362$274$9: 17",
-            "inFlight": "Pangea Airlines$632$282$16: 37",
-            "reserveFlights.x": 53,
-            "reserveFlights.y": 7
-        }
-        response = self.client.post("/mercurypurchase.php", name="Select a flight", data=payload)
+        response = self.client.post(
+            "/mercurypurchase.php",
+            name="Select a flight",
+            data={
+                "fromPort": "Acapulco",
+                "toPort": "Acapulco",
+                "passCount": 1,
+                "toDay": 12,
+                "toMonth": 7,
+                "fromDay": 12,
+                "fromMonth": 7,
+                "servClass": "Coach",
+                "outFlight": "Pangea Airlines$362$274$9: 17",
+                "inFlight": "Pangea Airlines$632$282$16: 37",
+                "reserveFlights.x": 53,
+                "reserveFlights.y": 7
+            })
         assert response.status_code == HTTPStatus.OK, "Failed to select a flight"
         assert verify_response_time(response, 1), "Request to select a flight took more than 1 second"
 
     @task
     def book_flight(self):
         print("Perform request to book flight")
-        payload = {
-            "outFlightName": "Pangea Airlines", "outFlightNumber": "362", "outFlightPrice": "274", "outFlightTime": "9:17",
-            "inFlightName": "Pangea Airlines", "inFlightNumber": "632", "inFlightPrice": "282", "inFlightTime": "16:37",
-            "fromPort": "Acapulco", "toPort": "Acapulco", "passCount": "1",
-            "toDay": "12", "toMonth": "7", "fromDay": "12", "fromMonth": "7",
-            "servClass": "Coach",
-            "subtotal": "556", "taxes": "45",
-            "passFirst0": "Andrii", "passLast0": "Test",
-            "creditCard": "AX", "creditnumber": "12345",
-            "cc_exp_dt_mn": "01",  "cc_exp_dt_yr": "2010", "cc_frst_name": "Andrii", "cc_last_name": "Test",
-            "billAddress1": "1325 Borregas Ave.", "billCity": "Sunnyvale", "billState": "CA", "billZip": "94089",
-            "billCountry": "215",
-            "delAddress1": "1325 Borregas Ave.", "delCity": "Sunnyvale", "delState": "CA", "delZip": "94089",
-            "delCountry": "215",
-            "buyFlights.x": "46", "buyFlights.y": "13"
-        }
-        response = self.client.post("/mercurypurchase2.php", name="Book a flight", data=payload)
+
+        response = self.client.post(
+            "/mercurypurchase2.php",
+            name="Book a flight",
+            data={
+                "outFlightName": "Pangea Airlines", "outFlightNumber": "362", "outFlightPrice": "274",
+                "outFlightTime": "9:17",
+                "inFlightName": "Pangea Airlines", "inFlightNumber": "632", "inFlightPrice": "282",
+                "inFlightTime": "16:37",
+                "fromPort": "Acapulco", "toPort": "Acapulco", "passCount": "1",
+                "toDay": "12", "toMonth": "7", "fromDay": "12", "fromMonth": "7",
+                "servClass": "Coach",
+                "subtotal": "556", "taxes": "45",
+                "passFirst0": "Andrii", "passLast0": "Test",
+                "creditCard": "AX", "creditnumber": "12345",
+                "cc_exp_dt_mn": "01", "cc_exp_dt_yr": "2010", "cc_frst_name": "Andrii", "cc_last_name": "Test",
+                "billAddress1": "1325 Borregas Ave.", "billCity": "Sunnyvale", "billState": "CA", "billZip": "94089",
+                "billCountry": "215",
+                "delAddress1": "1325 Borregas Ave.", "delCity": "Sunnyvale", "delState": "CA", "delZip": "94089",
+                "delCountry": "215",
+                "buyFlights.x": "46", "buyFlights.y": "13"
+            })
         assert response.status_code == HTTPStatus.OK, "Failed to book a flight"
         assert verify_response_time(response, 1), "Request to book a flight took more than 1 second"
 
--- test/insurance.py
+++ test/insurance.py
@@ -22,15 +22,18 @@ class UserBehaviour(TaskSet):
         self.update_view_state(launch_res)
 
         print("Perform login")
-        payload = {
-            "login-form": "login-form",
-            "login-form:email": "andriitest@fakemail.com",
-            "login-form:password": "locusttest",
-            "login-form:login.x": "57",
-            "login-form:login.y": "9",
-            "javax.faces.ViewState": self.view_state
-        }
-        login_res = self.client.post("/index.jsf", name="Login", data=payload, cookies=self.cookies)
+        login_res = self.client.post(
+            "/index.jsf",
+            data={
+                "login-form": "login-form",
+                "login-form:email": "andriitest@fakemail.com",
+                "login-form:password": "locusttest",
+                "login-form:login.x": "57",
+                "login-form:login.y": "9",
+                "javax.faces.ViewState": self.view_state
+            },
+            cookies=self.cookies,
+            name="Login")
         assert login_res.status_code == HTTPStatus.OK, "Failed to login"
         self.cookies["UserSessionFilter.sessionId"] = login_res.cookies["UserSessionFilter.sessionId"]
         self.update_view_state(login_res)
@@ -48,68 +51,76 @@ class UserBehaviour(TaskSet):
         @task
         def complete_step_1(self):
             print("Complete step 1 of Auto Quote")
-            payload = {
-                "autoquote": "autoquote",
-                "autoquote:zipcode": "10101",
-                "autoquote:e-mail": "andriitest@fakemail.com",
-                "autoquote:vehicle": "car",
-                "autoquote:next.x": "54",
-                "autoquote:next.y": "9",
-                "javax.faces.ViewState": self.parent.view_state
-            }
-            response = self.client.post("/quote_auto.jsf", name="Auto Quote step 1", cookies=self.parent.cookies,
-                                        data=payload)
+            response = self.client.post(
+                "/quote_auto.jsf",
+                data={
+                    "autoquote": "autoquote",
+                    "autoquote:zipcode": "10101",
+                    "autoquote:e-mail": "andriitest@fakemail.com",
+                    "autoquote:vehicle": "car",
+                    "autoquote:next.x": "54",
+                    "autoquote:next.y": "9",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Auto Quote step 1")
             assert response.status_code == HTTPStatus.OK, "Failed to complete Auto Quote step 1"
             self.parent.update_view_state(response)
 
         @task
         def complete_step_2(self):
             print("Complete step 2 of Auto Quote")
-            payload = {
-                "autoquote": "autoquote",
-                "autoquote:age": "030",
-                "autoquote:gender": "Male",
-                "autoquote:type": "Good",
-                "autoquote:next.x": "30",
-                "autoquote:next.y": "8",
-                "javax.faces.ViewState": self.parent.view_state
-            }
-            response = self.client.post("/quote_auto2.jsf", name="Auto Quote step 2", cookies=self.parent.cookies,
-                                        data=payload)
+            response = self.client.post(
+                "/quote_auto2.jsf",
+                data={
+                    "autoquote": "autoquote",
+                    "autoquote:age": "030",
+                    "autoquote:gender": "Male",
+                    "autoquote:type": "Good",
+                    "autoquote:next.x": "30",
+                    "autoquote:next.y": "8",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Auto Quote step 2")
             assert response.status_code == HTTPStatus.OK, "Failed to complete Auto Quote step 2"
             self.parent.update_view_state(response)
 
         @task
         def complete_step_3(self):
             print("Complete step 3 of Auto Quote")
-            payload = {
-                "autoquote": "autoquote",
-                "autoquote:year": "2018",
-                "makeCombo": "Toyota",
-                "autoquote:make": "Toyota",
-                "modelCombo": "Camry",
-                "autoquote:model": "Camry",
-                "autoquote:finInfo": "Own",
-                "autoquote:next.x": "35",
-                "autoquote:next.y": "15",
-                "javax.faces.ViewState": self.parent.view_state
-            }
-            response = self.client.post("/quote_auto3.jsf", name="Auto Quote step 3", cookies=self.parent.cookies,
-                                        data=payload)
+            response = self.client.post(
+                "/quote_auto3.jsf",
+                data={
+                    "autoquote": "autoquote",
+                    "autoquote:year": "2018",
+                    "makeCombo": "Toyota",
+                    "autoquote:make": "Toyota",
+                    "modelCombo": "Camry",
+                    "autoquote:model": "Camry",
+                    "autoquote:finInfo": "Own",
+                    "autoquote:next.x": "35",
+                    "autoquote:next.y": "15",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Auto Quote step 3")
             assert response.status_code == HTTPStatus.OK, "Failed to complete Auto Quote step 3"
             self.parent.update_view_state(response)
 
         @task
         def proceed_to_result(self):
             print("Proceed to Auto Quote result")
-            payload = {
-                "quote-result": "quote-result",
-                "autoquote:next.x": "38",
-                "autoquote:next.y": "12",
-                "javax.faces.ViewState": self.parent.view_state
-            }
-            response = self.client.post("/quote_result.jsf", name="Proceed to quote result",
-                                        cookies=self.parent.cookies, data=payload)
+            response = self.client.post(
+                "/quote_result.jsf",
+                data={
+                    "quote-result": "quote-result",
+                    "autoquote:next.x": "38",
+                    "autoquote:next.y": "12",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Proceed to quote result")
             assert response.status_code == HTTPStatus.OK, "Failed to proceed to quote result"
             self.parent.update_view_state(response)
 
@@ -117,16 +128,21 @@ class UserBehaviour(TaskSet):
         def purchase_quote(self):
             print("Purchase an auto quote")
             payload = {
-                "purchaseQuote": "purchaseQuote",
-                "purchaseQuote:cardname": "Andrii Test",
-                "purchaseQuote:cardnumber": "1294 4863 2356 3975",
-                "purchaseQuote:expiration": "02/09",
-                "purchaseQuote:purchase.x": "42",
-                "purchaseQuote:purchase.y": "9",
-                "javax.faces.ViewState": self.parent.view_state
+
             }
-            response = self.client.post("/purchase_quote.jsf", name="Purchase quote", cookies=self.parent.cookies,
-                                        data=payload)
+            response = self.client.post(
+                "/purchase_quote.jsf",
+                data={
+                    "purchaseQuote": "purchaseQuote",
+                    "purchaseQuote:cardname": "Andrii Test",
+                    "purchaseQuote:cardnumber": "1294 4863 2356 3975",
+                    "purchaseQuote:expiration": "02/09",
+                    "purchaseQuote:purchase.x": "42",
+                    "purchaseQuote:purchase.y": "9",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Purchase quote")
             assert response.status_code == HTTPStatus.OK, "Failed to purchase a quote"
 
         @task
@@ -139,7 +155,10 @@ class UserBehaviour(TaskSet):
         @task
         def select_agent_lookup(self):
             print("Select Agent lookup in menu")
-            response = self.client.get("/agent_lookup.jsf", name="Select agent lookup", cookies=self.parent.cookies)
+            response = self.client.get(
+                "/agent_lookup.jsf",
+                cookies=self.parent.cookies,
+                name="Select agent lookup")
             assert response.status_code == HTTPStatus.OK, "Failed to select agent lookup"
             assert "Find an Insurance Co. Agent" in response.text, "Agent lookup page is not open"
             self.parent.update_view_state(response)
@@ -147,14 +166,16 @@ class UserBehaviour(TaskSet):
         @task
         def search_all_agents(self):
             print("Perform search for all agents")
-            payload = {
-                "show-all": "show-all",
-                "show-all:search-all.x": "30",
-                "show-all:search-all.y": "10",
-                "javax.faces.ViewState": self.parent.view_state
-            }
-            response = self.client.post("/agent_lookup.jsf", name="Search all agents", cookies=self.parent.cookies,
-                                        data=payload)
+            response = self.client.post(
+                "/agent_lookup.jsf",
+                data={
+                    "show-all": "show-all",
+                    "show-all:search-all.x": "30",
+                    "show-all:search-all.y": "10",
+                    "javax.faces.ViewState": self.parent.view_state
+                },
+                cookies=self.parent.cookies,
+                name="Search all agents")
             assert response.status_code == HTTPStatus.OK, "Failed to search for all agents"
             assert "Insurance Agent Search Results" in response.text, "Search results table is not displayed"
 
